533fcfb5d1beb2aa5fa275d47212034d8461e4c4
michaelGit <m.tich22@seznam.cz>
2018-07-23 10:52:16 +0200

    Add repository class for entities

---
+++ src/AppBundle/Repository/CharacterRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michael
+ * Date: 23.7.18
+ * Time: 10:49
+ */
+
+namespace AppBundle\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class CharacterRepository extends EntityRepository
+{
+
+}
