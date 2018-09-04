ec75596c8da92be41cb5fe5dd0796e8512e12cd5
michaelGit <m.tich22@seznam.cz>
2018-09-04 17:41:23 +0200

    Add some js

--- web/js/buttonInput.js
+++ web/js/buttonInput.js
@@ -1,5 +1,31 @@
 $(document).foundation()
+
 var lastCommand = "";
+
+function getCommand(command){
+    var commands = {
+        "f" : "Forward",
+        "b" : "Back",
+        "u" : "Up",
+        "d" : "Down",
+        "1" : "1",
+        "2" : "2",
+        "3" : "3",
+        "4" : "4",
+        "ws" : "While standing",
+        "wr" : "While running",
+        "1+2" : "1+2",
+        "1+3" : "1+3",
+        "1+4" : "1+4",
+        "2+3" : "2+3",
+        "2+4" : "2+4",
+        "3+4" : "3+4",
+        "default" : "",
+    };
+
+    return commands[command] || commands["default"];
+}
+
 
 function setCommand(command){
     try{
@@ -14,11 +40,24 @@ function setCommand(command){
             && lastCommand !== "~"
             && lastCommand !== "*"
         ){
-            text +=", ";
+            text +=",";
         }
         text += command;
         document.getElementById('move_command').value += text;
         lastCommand = command;
+        convertCommand(document.getElementById('move_command').value);
+    }catch (e) {
+        alert(e);
+    }
+}
+
+function convertCommand(text){
+    try{
+        var commands = text.split(',');
+        document.getElementById("show_command").innerHTML = "";
+        for (i = 0; i < commands.length; i++){
+            document.getElementById("show_command").innerHTML += getCommand(commands[i]);
+        }
     }catch (e) {
         alert(e);
     }
@@ -40,6 +79,9 @@ function setHitLevel(hitLevel){
 function clearField(field){
     try{
         document.getElementById(field).value = "";
+        if (field === "move_command"){
+            document.getElementById("show_command").innerHTML = "";
+        }
     }catch (e) {
         alert(e);
     }
